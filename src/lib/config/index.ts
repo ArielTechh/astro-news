4f3d0ccfac27470cb8122f0e1f6ae0974311229d
WBScharaph <webuildsite.wbs@gmail.com>
2025-08-04 00:06:10 +0300

    screaming frog error

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,8 +1,8 @@
 import type { Link } from "../types";
 
 export const SITE = {
-  title: "TechHorizons",
-  description: "A news website built with Astro",
+  title: "TechHorizons – חדשות טכנולוגיה, חדשנות וסטארטאפים בישראל 2025",
+  description: "התעדכנו בחדשות הטכנולוגיה האחרונות עם TechHorizons – סקירות סמארטפונים, גאדג'טים, משחקים, מדע וחדשנות בישראל לשנת 2025. כל מה שחדש, במקום אחד.",
   author: "TechHorizons",
   url: "https://techhorizons.co.il", // ← AJOUTEZ www
   locale: "he-IL",
@@ -18,18 +18,18 @@ export const OTHER_LINKS: Link[] = [
     href: "/about",
     text: "אודות",
   },
-  {
-    href: "/contact",
-    text: "צור קשר",
-  },
-  {
-    href: "/Accessibility_Statement",
-    text: "הצרת נגישות",
-  },
-  {
-    href: "/cookie-policy",
-    text: "Cookie Policy",
-  },
+  // {
+  //   href: "/contact",
+  //   text: "צור קשר",
+  // },
+  // {
+  //   href: "/Accessibility_Statement",
+  //   text: "הצרת נגישות",
+  // },
+  // {
+  //   href: "/cookie-policy",
+  //   text: "Cookie Policy",
+  // },
 ];
 
 
