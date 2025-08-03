3239c3775dc7d66a5943e871cb6cd87708d3e50f
WBScharaph <webuildsite.wbs@gmail.com>
2025-08-04 01:02:10 +0300

    screaming frog error

--- astro.config.mjs
+++ astro.config.mjs
@@ -177,10 +177,6 @@ export default defineConfig({
   site: SITE.url,
   base: SITE.basePath,
   trailingSlash: 'never',
-  redirects: {
-    // ✅ AJOUTEZ CETTE LIGNE POUR ÉVITER LA REDIRECTION AUTO
-    '/categories/[category]': '/categories/[category]'
-  },
   markdown: {
     remarkPlugins: [readingTime, modifiedTime],
   },
